57eefa91b639e2a423df313c5da103f87d8e99db
Jack DeVincens <78175287+jackdevincens@users.noreply.github.com>
2023-04-20 12:50:28 -0400

    Initial Commit

---
+++ SatelliteProject/SatelliteProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SatelliteProjectApp.swift
+//  SatelliteProject
+//
+//  Created by Jack Marshall DeVincens on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SatelliteProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
